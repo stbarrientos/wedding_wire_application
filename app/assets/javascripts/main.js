0ae0761b365b9c01223bef102f38583921f71a2f
stbarrientos <stbarrientos@gmail.com>
2014-10-18 17:11:40 -0400

    finished ajax call

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,21 +1,51 @@
-$(document).ready(function(){
+var Finder = {
 
-$("#search-form").on('submit', function(event){
-	event.preventDefault();
-	var location = $("#location").val();
-	getStores(location);
-});
+	initialize: function(){
+		var self = this;
+		$("form").on('submit', function(event){
+			event.preventDefault();
+			var city = $("#city").val();
+			var state = $("select option:selected").val();
+			var location = city + ", " + state;
+			console.log(location);
+			self.getStores(location);
+			$("#city").val("");
+			$("select option:eq(0)").prop("selected", true);;
+		});
+	},
 
-function getStores(location){
-	$.ajax({
-		url: "http://api.yelp.com/v2/search?term=coffee&limit=10&oauth_token=2VRHs5xCwuxFMOt75QZcfGA40NMe4e9o&oauth_consumer_key=siqb9GMY4J8dtVU0haVjOA&location=" + location,
-		dataType: "jsonp",
-		method: "GET",
-		success: function(data){
-			console.log(data)
-		}
-	});
+	getStores: function(location){
+		self = this
+		var htmlString = "<p>Results:</p><ul>"
+		$.ajax({
+			url: "https://api.foursquare.com/v2/venues/search?near=" + location + "&limit=10&query=coffee&client_id=" + Secrets.fourSquareClientId + "&client_secret=" + Secrets.fourSquareClientSecret +"&v=20141010",
+			dataType: "jsonp",
+			method: "GET",
+			success: function(data){
+				for (var i = 0; i < data.response.venues.length; i ++){
+					var venue = data.response.venues[i]
+					var address = venue.location.formattedAddress.reduce(function(a,b){
+						a += " "
+						return a.concat(b);
+					});
+					htmlString += "<li>" + venue.name + " - " + address + "</li>";
+				}
+				htmlString += "</ul>";
+				self.render(htmlString);
+			}
+		});
+	},
+
+	render: function(htmlString){
+		this.clearResults();
+		$(".results").append(htmlString)
+	},
+
+	clearResults: function(){
+		$(".results").html("");
+	}
 }
 
-
+$(document).ready(function(){
+	Finder.initialize();
 });
---
+++ app/assets/javascripts/secrets.js
@@ -0,0 +1,8 @@
+var Secrets = {
+	yelpConsumerKey: "siqb9GMY4J8dtVU0haVjOA",
+	yelpConsumerSecret: "_fDU-Rq68-zC5Hw4jMJ5U_7kNdw",
+	yelpToken: "2VRHs5xCwuxFMOt75QZcfGA40NMe4e9o",
+	yelpTokenSecret: "YnG_6VXzKq24AVBrXp5NRgJ5eNI",
+	fourSquareClientId: "EBSOJ5HFUE5SOP5ZPWO3GIM23334KAZEJE31AI25BPNUVQSY",
+	fourSquareClientSecret: "4IXFLUYZAJW03XOL44YRW32VBWUESGP3ASSIUPQQ1WIFQLSQ"
+}
